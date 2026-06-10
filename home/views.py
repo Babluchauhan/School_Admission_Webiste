8cef5b248631ce57bbde876eec320515b0c30545
Babluchauhan <babluchauhan926@gmail.com>
2026-06-10 14:05:05 +0530

    created signup page

--- home/views.py
+++ home/views.py
@@ -1,15 +1,48 @@
 from django.shortcuts import render, redirect, HttpResponse
 from django.contrib import messages
 from .models import Admission
+from .forms import RegisterForm
+from django.contrib.auth import authenticate, login as auth_login
+from django.contrib.auth.models import User
 
 def home(request):
     return render(request, 'home.html')
 
 def login(request):
+    # if request.method == 'POST':
+    #     # Get email/username and password from form
+    #     username = request.POST.get('email')  # or you can name it 'username'
+    #     password = request.POST.get('password')
+        
+    #     # Authenticate the user
+    #     user = authenticate(request, username=username, password=password)
+        
+    #     if user is not None:
+    #         # If user exists and password is correct, log them in
+    #         auth_login(request, user)
+    #         messages.success(request, f'Welcome {user.first_name}!')
+    #         return redirect('home')  # redirect to home page
+    #     else:
+    #         # If authentication fails, show error
+    #         messages.error(request, 'Invalid email or password')
+    #         return render(request, 'login.html')
+    
+    # # If GET request, just show the login form
     return render(request, 'login.html')
 
 def signup(request):
-    return render(request, 'signup.html')
+    if request.method == 'POST': # when user submits the form
+        form = RegisterForm(request.POST)
+
+        if form.is_valid(): # if form is valid then save the user
+            user = form.save()     # save user in DB
+            # login(request, user)   # log user in
+            return redirect('login') # redirect to login page after successful registration
+
+    else:
+        form = RegisterForm()
+
+    return render(request, 'signup.html', {'form': form})
 
 def admission_form(request):
     #Fix this form make it short and simple
